1a339b3d60e6a2671a0765722d6bb635486cabb0
Danny Bowman <danny@d-bow.com>
2015-12-25 19:50:10 -0500

    Support dynamic image URLs and giphy queries for base image

--- server.js
+++ server.js
@@ -1,42 +1,118 @@
 var fs = require('fs');
+var http = require('http');
+var https = require('https');
+var url = require('url');
 
 var express = require('express');
 var gm = require('gm');
+var validUrl = require('valid-url');
+var giphyApi = require('giphy-api');
 
 var app = express();
+var giphy = giphyApi();
 
 var bowmeme = gm('dbow.png');
 
-app.get('/', function(req, res) {
-  // TODO(dbow): Dynamically get base image.
-  var baseImage = fs.createReadStream('chilltank.png');
+
+function getBaseImageUrl(req, res, next) {
+  if (validUrl.isUri(req.query.u)) {
+    req.baseImageUrl = req.query.u;
+    return next();
+  }
+
+  giphy.search({
+      q: req.query.u,
+      limit: 1
+  }, function(err, res) {
+    if (res.data.length) {
+      req.baseImageUrl = res.data[0].images.original.url;
+    } else {
+      req.baseImageUrl = 'http://i.giphy.com/tpwwhv1BLd31e.gif';
+    }
+    next();
+  });
+}
+
+function getImage(req, res, next) {
+  var imageUrl = url.parse(req.baseImageUrl);
+  var request = imageUrl.protocol === 'https:' ? https.request : http.request;
+  var imageRequest = request(imageUrl, function(imageResponse) {
+    var baseImage = 'base_' + Math.random();
+    var stream = fs.createWriteStream(baseImage);
+    imageResponse.pipe(stream);
+    imageResponse.on('end', function() {
+      req.baseImage = baseImage;
+      next();
+    });
+  });
+  imageRequest.on('error', function(e) {
+    // TODO(dbow): Handle errors.
+  });
+  imageRequest.end();
+}
+
+function composite(req, res, next) {
+  var baseImage = fs.createReadStream(req.baseImage);
 
   gm(baseImage)
-    .size({bufferStream: true}, function(err, size) {
+    .identify({bufferStream: true}, function (err, data) {
+      if (err) {
+        return res.send('Something went horribly wrong!:' + err.message);
+      }
+
       var self = this;
+
+      var width = data.size.width;
+      var height = data.size.height;
+
+      req.format = data.format;
 
       var resizedBowmeme = 'temp_' + Math.random();
 
       // Resize bowmeme to fit baseImage.
       bowmeme
-        .resize(size.width, size.height)
+        .resize(width, height)
         .write(resizedBowmeme, function(err) {
+          gm(resizedBowmeme)
+            .size(function(err, size) {
 
-          // Composite resized bowmeme
-          self.composite(resizedBowmeme)
-          self.toBuffer('PNG', function(err, buffer) {
+              var bWidth = size.width / 2;
+              var bHeight = size.height / 2;
 
-            // Delete temp resized bowmeme.
-            fs.unlink(resizedBowmeme);
+              var compositeString = [
+                'image over ',
+                width - bWidth,
+                ',',
+                height - bHeight,
+                ' ',
+                bWidth,
+                ',',
+                bHeight,
+                ' ',
+                resizedBowmeme
+              ];
 
-            // Return composited image.
-            res.setHeader('content-type', 'image/png');
-            res.end(buffer);
-          })
+              // Composite resized bowmeme
+              self
+                .command('convert')
+                .in('-coalesce')
+                .out('-resize', [width, 'x', height].join(''))
+                .out('-draw', compositeString.join(''))
+                .toBuffer(function(err, buffer) {
+                  req.composite = buffer;
+                  fs.unlink(resizedBowmeme);
+                  fs.unlink(req.baseImage);
+                  next();
+                });
+            });
         });
     });
-});
+}
 
+app.get('/', getBaseImageUrl, getImage, composite, function(req, res) {
+  res.setHeader('content-type', 'image/' + (req.format || 'png'));
+  res.end(req.composite);
+});
 
 var server = app.listen(3000, function () {
   var host = server.address().address;
